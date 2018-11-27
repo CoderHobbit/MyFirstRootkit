fa99eb87f3b2a4affc0400b734c38bd9dc38f208
Dalyn <wernergwen@gmail.com>
2018-11-27 15:53:23 -0800

    music

---
+++ playSound.cpp
@@ -0,0 +1,13 @@
+// play sound
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std:
+
+int main()
+{
+	cout << "starting" << endl;
+	Payload("C:\Users\Dalyn\Music\NyanCat.wav", NULL, SND_FILENAME):
+	return 0:
+}
